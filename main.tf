9e01670b6b5f7cebc988655ddf17d072690b7973
rgustafs <rgustafsson@hashicorp.com>
2019-05-06 07:49:16 +0200

    0.1

---
+++ main.tf
@@ -0,0 +1,75 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "rgustafsson"
+
+    workspaces {
+      name = "gcp-compute"
+    }
+  }
+}
+
+#variable "gcp_credentials" {
+#  description = "GCP credentials needed by google provider"
+#}
+
+variable "gcp_project" {
+  description = "GCP project name"
+}
+
+variable "gcp_region" {
+  description = "GCP region, e.g. us-east1"
+  default = "us-east1"
+}
+
+variable "gcp_zone" {
+  description = "GCP zone, e.g. us-east1-a"
+  default = "us-east1-b"
+}
+
+variable "machine_type" {
+  description = "GCP machine type"
+  default = "n1-standard-1"
+}
+
+variable "instance_name" {
+  description = "GCP instance name"
+  default = "robert_demo"
+}
+
+variable "image" {
+  description = "image to build instance from"
+  default = "debian-cloud/debian-9"
+}
+
+provider "google"{}
+#provider "google" {
+#  credentials = "${var.gcp_credentials}"
+#  project     = "${var.gcp_project}"
+#  region      = "${var.gcp_region}"
+#}
+
+resource "google_compute_instance" "demo" {
+  name         = "${var.instance_name}"
+  machine_type = "${var.machine_type}"
+  zone         = "${var.gcp_zone}"
+
+  boot_disk {
+    initialize_params {
+      image = "${var.image}"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+}
+
+output "external_ip"{
+  value = "${google_compute_instance.demo.network_interface.0.access_config.0.assigned_nat_ip}"
+}
